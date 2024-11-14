05e6d40dbd383f2757d074907b96b113693b82e7
sahilnale <sahilnale@icloud.com>
2024-11-13 22:31:22 -0800

    new attempt

--- FindMyFood/Views/Features/MapView.swift
+++ FindMyFood/Views/Features/MapView.swift
@@ -1,49 +1,52 @@
 import SwiftUI
 import MapKit
+import CoreLocation
 
-//struct MapView: View {
-//    @State private var region = MKCoordinateRegion(
-//        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
-//        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-//    )
-//    
-//    var body: some View {
-//        Map(coordinateRegion: $region)
-//            .edgesIgnoringSafeArea(.all)
-//            .overlay(
-//                Button(action: {
-//                    // Add new post
-//                }) {
-//                    Image(systemName: "plus.circle.fill")
-//                        .font(.system(size: 44))
-//                        .foregroundColor(.accentColor)
-//                        .background(Color.white)
-//                        .clipShape(Circle())
-//                        .shadow(radius: 4)
-//                }
-//                .padding(),
-//                alignment: .bottomTrailing
-//            )
-//    }
-//}
-
-
-class MapViewModel: UIViewController {
+class MapViewModel: UIViewController, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
     private let map: MKMapView = {
         let map = MKMapView()
         map.showsUserLocation = true
-        map.userTrackingMode = .followWithHeading // Keeps the user's location centered on the map
+        map.userTrackingMode = .followWithHeading
         return map
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set up the map view
         view.addSubview(map)
+        
+        // Configure the location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         map.frame = view.bounds
+    }
+    
+    // CLLocationManagerDelegate method
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        // Center the map on the user's current location
+        let region = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: 1000,
+            longitudinalMeters: 1000
+        )
+        map.setRegion(region, animated: true)
+        
+        // Stop updating location to save battery life
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get location: \(error.localizedDescription)")
     }
 }
 
@@ -56,4 +59,3 @@ struct MapView: UIViewControllerRepresentable {
         // Handle any updates to the view controller here
     }
 }
-
