98c1bcf1df6e84f59332cf47f073d2f9552d0f88
Rithvik Dirisala <rithvikdirisala@Rithviks-MacBook-Pro.local>
2024-11-16 23:30:55 -0800

    route troubleshooting

--- FindMyFood/Networking/NetworkManager.swift
+++ FindMyFood/Networking/NetworkManager.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class NetworkManager {
     static let shared = NetworkManager()
-    private let baseURL = "http://localhost:8080/api/v1/"
+    private let baseURL = "http://localhost:8080/api/v1"
     
     private init() {}
     
